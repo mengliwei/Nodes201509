0d21b02233996a1ba4263640e794948b90f3624a
mengliwei <2458423458@qq.com>
2016-02-17 20:17:10 +0800

    老师讲义20170217

--- 20160127_全日制第四周第二天/可以删除的库2.js
+++ 20160127_全日制第四周第二天/可以删除的库2.js
@@ -1,8 +1,7 @@
 (function () {
-    //getCss
     function getCss(curEle, attr) {
         var val = reg = null;
-        if ("getComputedStyle"in window) {
+        if (window.getComputedStyle) {
             val = window.getComputedStyle(curEle, null)[attr];
         } else {
             if (attr === "opacity") {
@@ -13,13 +12,12 @@
                 val = curEle.currentStyle[attr];
             }
         }
-        reg = /^-?\d+(\.\d+)?(px|pt|em|rem)$/;
+        reg = /^-?\d+(\.\d+)?(px|pt|pe|rem)$/;
         return reg.test(val) ? parseFloat(val) : val;
     }
 
-    //setCss
     function setCss(curEle, attr, value) {
-        var reg = /^(width|height|(padding|margin(Top|Left|Right|Bottom))|top|left|right|bottom)$/;
+        var reg = /^(width|height|top|bottom|left|right|(margin|padding(Right|Left|Top|Bottom)?))$/;
         if (attr === "float") {
             curEle["style"]["cssFloat"] = value;
             curEle["style"]["styleFloat"] = value;
@@ -28,46 +26,15 @@
                 curEle["style"]["opacity"] = value;
                 curEle["style"]["filter"] = "alpha(opacity=" + value * 100 + ")";
             }
-        } else if (reg.test(attr)) {
-            curEle["style"][attr] = isNaN(value) ? value : value + "px";
         } else {
-            curEle["style"][attr] = value;
-        }
-    }
-
-    //动画公式
-    function Liner(t, b, c, d) {
-        return c * t / d + b;
-    }
-
-    //匀速
-    function animate(curEle, tarObj, duration, effect, callback) {
-        var fnEffect = Liner();
-        var times = 0, beginObj = {}, changObj = {};
-        for (var key in tarObj) {
-            if (tarObj.hasOwnProperty(key)) {
-                beginObj[key] = getCss(curEle, key);
-                changObj[key] = tarObj[key] - beginObj[key];
+            if (reg.test(attr)) {
+                curEle["style"][attr] = isNaN(value) ? value : value + "px";
             }
         }
-        window.clearInterval(curEle.timer);
-        curEle.timer = window.setInterval(function () {
-            times += 10;
-            if (times >= duration) {
-                window.clearInterval(curEle.timer);
-                for (var key in tarObj) {
-                    if (tarObj.hasOwnProperty(key)) {
-                        setCss(curEle, key, tarObj[key]);
-                    }
-                }
-            }
-            for(var key in changObj){
-                if(changObj.hasOwnProperty(key)){
-                    var cur=Liner(times,beginObj[key],changObj[key],duration);
-                    setCss(curEle,key,cur);
-                }
-            }
-        }, 10)
+        curEle["style"][attr] = value;
     }
-    window.animate=animate;
-})();
+function Liner(){
+
+}
+})
+();
